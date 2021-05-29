a9bad8e3bb418c3136c99f85b56c60b1b2ea9090
dsteoh <desiangteoh@gmail.com>
2021-05-29 11:53:42 +1000

    Setting up data models

---
+++ shared/CatalogMigrations.DataModels/Models/SupplierProductBarcode.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogMigrations.DataModels.Models
+{
+    public class SupplierProductBarcode
+    {
+        public int SupplierId { get; set; }
+        public string Sku { get; set; }
+        public string Barcode { get; set; }
+    }
+}
