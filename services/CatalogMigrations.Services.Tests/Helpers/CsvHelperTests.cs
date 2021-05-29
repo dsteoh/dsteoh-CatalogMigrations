438809812135d621ed2bc3c6a123e213806d53a0
dsteoh <desiangteoh@gmail.com>
2021-05-29 12:13:58 +1000

    Setup CSV tests with test data

---
+++ services/CatalogMigrations.Services.Tests/Helpers/CsvHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace CatalogMigrations.Services.Tests.Helpers
+{
+    public class CsvHelperTests
+    {
+        [Fact]
+        public void ShouldDeserializeCsvToList()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Fact]
+        public void ShouldConvertListToCsv()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Fact]
+        public void ShouldContinueToPopulateWhenErrorIsThrown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
