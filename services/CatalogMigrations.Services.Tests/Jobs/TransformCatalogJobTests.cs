ae0bcfa56a61bdb0e3f04e24602e104ec0d4b3ee
dsteoh <desiangteoh@gmail.com>
2021-05-29 20:51:21 +1000

    Super catalog job (#4)
    * setup transformCatalogJob tests
    
    * Implemented method to return product from matching barcode

---
+++ services/CatalogMigrations.Services.Tests/Jobs/TransformCatalogJobTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatalogMigrations.DataModels.Models;
+using CatalogMigrations.Services.Helpers.Csv;
+using CatalogMigrations.Services.Jobs;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CatalogMigrations.Services.Tests.Jobs
+{
+    public class TransformCatalogJobTests
+    {
+        private TransformCatalogJob _transformCatalogJob;
+        
+        public TransformCatalogJobTests()
+        {
+            _transformCatalogJob = new TransformCatalogJob();
+        }
+
+        [Fact]
+        public void TransformCatalog_ShouldReturn_SuperCatalog()
+        {
+            
+        }
+        
+        [Fact]
+        public void GetMatchingBarcode_ShouldReturn_MatchingBarcodes()
+        {
+            var barcodeA = new List<SupplierProductBarcode>()
+            {
+                new()
+                {
+                    Sku = "1111-1111-1111",
+                    Barcode = "z2783613083817",
+                    SupplierId = 1
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083800",
+                    SupplierId = 1
+                }
+            };
+            
+            var barcodeB = new List<SupplierProductBarcode>()
+            {
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083818",
+                    SupplierId = 2
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083800",
+                    SupplierId = 1
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083801",
+                    SupplierId = 1
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613093800",
+                    SupplierId = 1
+                }
+            };
+
+            var matchedProducts = _transformCatalogJob.GetMatchingBarcode(barcodeA, barcodeB);
+            matchedProducts.Single().Barcode.Should().Be("z2783613083800");
+        }
+    }
+}
--- services/CatalogMigrations.Services/Jobs/TransformCatalogJob.cs
+++ services/CatalogMigrations.Services/Jobs/TransformCatalogJob.cs
@@ -1,7 +1,42 @@
-﻿namespace CatalogMigrations.Services.Jobs
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using CatalogMigrations.DataModels.Models;
+using CatalogMigrations.Services.Helpers.Csv;
+using CatalogMigrations.Services.Helpers.Csv.Mappers;
+
+namespace CatalogMigrations.Services.Jobs
 {
-    public class TransformCatalogJob
+    public interface ITransformCatalogJob
     {
         
+    }
+    public class TransformCatalogJob : ITransformCatalogJob
+    {
+        public TransformCatalogJob()
+        {
+          
+        }
+
+        public void TransformCatalog(List<Catalog> catalogs)
+        {
+            
+        }
+
+        public List<SupplierProductBarcode> GetMatchingBarcode(List<SupplierProductBarcode> supplierProductBarcodesA, List<SupplierProductBarcode>supplierProductBarcodesB)
+        {
+            var productList = new List<SupplierProductBarcode>();
+            
+            var resultSet = supplierProductBarcodesA
+                .Select(_ => _.Barcode)
+                .Intersect(supplierProductBarcodesB.Select(_ => _.Barcode)).ToList();
+
+            foreach (var barcode in resultSet)
+            {
+                var product = supplierProductBarcodesA.SingleOrDefault(_ => _.Barcode == barcode);
+                productList.Add(product);
+            }
+            return productList;
+        }
     }
 }
