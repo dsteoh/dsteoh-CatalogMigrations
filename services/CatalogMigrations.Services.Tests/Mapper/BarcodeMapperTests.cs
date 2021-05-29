a2f547654bc5fb39feac7681addf424c4adfc779
dsteoh <desiangteoh@gmail.com>
2021-05-29 20:55:00 +1000

    Merge into Develop Barcode mapper (#5)
    * setup transformCatalogJob tests
    
    * Implemented method to return product from matching barcode
    
    * Barcode mapper tests
    
    * Barcode mapper implementation
    
    * remove barcode implementation from transformcatalogjob

---
+++ services/CatalogMigrations.Services.Tests/Mapper/BarcodeMapperTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatalogMigrations.DataModels.Models;
+using CatalogMigrations.Services.Helpers.Csv;
+using CatalogMigrations.Services.Jobs;
+using CatalogMigrations.Services.Mapper;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CatalogMigrations.Services.Tests.Mapper
+{
+    public class BarcodeMapperTests
+    {
+        private BarcodeMapper _barcodeMapper;
+
+        public BarcodeMapperTests()
+        {
+            _barcodeMapper = new BarcodeMapper();
+        }
+        
+        [Fact]
+        public void GetMatchingBarcode_ShouldReturn_MatchingBarcodes()
+        {
+            var barcodeA = new List<SupplierProductBarcode>()
+            {
+                new()
+                {
+                    Sku = "1111-1111-1111",
+                    Barcode = "z2783613083817",
+                    SupplierId = 1
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083800",
+                    SupplierId = 1
+                }
+            };
+            
+            var barcodeB = new List<SupplierProductBarcode>()
+            {
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083818",
+                    SupplierId = 2
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083800",
+                    SupplierId = 1
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613083801",
+                    SupplierId = 1
+                },
+                new()
+                {
+                    Sku = "2222-1111-1111",
+                    Barcode = "z2783613093800",
+                    SupplierId = 1
+                }
+            };
+
+            var matchedProducts = _barcodeMapper.GetMatchingProducts(barcodeA, barcodeB);
+            matchedProducts.Single().Barcode.Should().Be("z2783613083800");
+        }
+    }
+}
